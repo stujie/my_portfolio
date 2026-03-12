7aff36ddd09f37272e5eca7e027b81e770d2da86
Stutie <stutiebanerjie@gmail.com>
2026-03-11 20:30:17 -0400

    Delete data_cleaning.py

--- Case Study 1 - Mental Health & Tech/data_cleaning.py
+++
@@ -1,7 +0,0 @@
-"""
-Name: Stutie Banerjie
-Date: 3.9.2026
-"""
-import pandas as pd
-
-df = pd.read_csv(r'C:\Users\Stutie Banerjie\OneDrive\Documents\Projects\VSCode\my_portfolio\Case Study 1 - Mental Health & Tech\survey.csv')
